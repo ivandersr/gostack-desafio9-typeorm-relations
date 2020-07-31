56e10a12656356696c23473ca728adf56760f0fb
Ivander <ivandersalvadorruiz@gmail.com>
2020-07-31 12:06:48 -0300

    Challenge complete

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -3,6 +3,7 @@ import { getRepository, Repository, In } from 'typeorm';
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
 import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
+import AppError from '@shared/errors/AppError';
 import Product from '../entities/Product';
 
 interface IFindProducts {
@@ -21,21 +22,64 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({
+      where: { name },
+    });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productIds = products.map(product => product.id);
+    const findProducts = await this.ormRepository.find({
+      id: In(productIds),
+    });
+
+    return findProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const updateProduct = products.map(async product => {
+      let existingProduct = await this.ormRepository.findOne(product.id);
+      if (!existingProduct) {
+        throw new AppError(
+          `The product with id ${product.id} does not exist`,
+          404,
+        );
+      } else {
+        const newQuantity = existingProduct.quantity - product.quantity;
+        if (newQuantity < 0) {
+          throw new AppError(
+            `Not enought of the product "${existingProduct.name} in stock"`,
+            400,
+          );
+        }
+        existingProduct = {
+          ...existingProduct,
+          quantity: newQuantity,
+        };
+      }
+      return existingProduct;
+    });
+
+    const updatedProducts = await Promise.all(updateProduct);
+    await this.ormRepository.save(updatedProducts);
+
+    return updatedProducts;
   }
 }
 
